c8f806768684ac8eb5f06585886edb3032928ea3
marcin_private1@wp.pl <marcin_private1@wp.pl>
2018-02-18 22:35:56 +0100

    Added functional logic

--- express/public/js-dev/logic.js
+++ express/public/js-dev/logic.js
@@ -1,22 +1,23 @@
 'use strict';
 
 var jsHook = {
-    GET_DATA: '.js-get-data'
+    SET_DATA: '.js-set-data'
 };
-var param = { "provider": "anonymous", "uid": "4a8cd039-0570-46b5-a2d1-22df3172bd36" };
+//var param = { "provider": "anonymous", "uid": "4a8cd039-0570-46b5-a2d1-22df3172bd36" };
 
-$(jsHook.GET_DATA).on('click', getData);
+var database = firebase.database();
+var name = '';
 
-function getData() {
-    $.ajax({
-        type: "GET",
-        url: "https://nodeexpress-fc15e.firebaseio.com/",
-        data: JSON.stringify(param)
-    }).done(showData);
-}
+$(jsHook.SET_DATA).on('click', setData);
 
-//info.html:1 Failed to load https://console.firebase.google.com/project/nodeexpress-fc15e/database/data/: Redirect from 'https://console.firebase.google.com/project/nodeexpress-fc15e/database/data/' to 'https://accounts.google.com/ServiceLogin?passive=1209600&osid=1&continue=https://console.firebase.google.com/project/nodeexpress-fc15e/database/data/&followup=https://console.firebase.google.com/project/nodeexpress-fc15e/database/data/' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource. Origin 'null' is therefore not allowed access.
+function setData() {
+    name = $('#name-input').val().trim();
 
-function showData(data) {
-    console.log(data);
-}
+    database.ref().set({
+        name: name
+    });
+};
+
+database.ref().on("value", function(snap) {
+    $('#display').html(snap.val().name);
+});
