0ae93b2480d421eb9d3c74334213ab247a131677
marcin_private1@wp.pl <marcin_private1@wp.pl>
2018-02-19 08:17:38 +0100

    Added auth

--- express/public/js-dev/logic.js
+++ express/public/js-dev/logic.js
@@ -3,7 +3,18 @@
 var jsHook = {
     SET_DATA: '.js-set-data'
 };
+
+//https://firebase.google.com/docs/auth/admin/create-custom-tokens
+
 //var param = { "provider": "anonymous", "uid": "4a8cd039-0570-46b5-a2d1-22df3172bd36" };
+var token_uid = '4a8cd039-0570-46b5-a2d1-22df3172bd36';
+
+firebase.auth().signInWithCustomToken(token_uid).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+});
 
 var database = firebase.database();
 var name = '';
